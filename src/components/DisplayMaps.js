8466d78196ec2858b037282a3d4b017ba9345a6a
hgozutok <hgozutok@gmail.com>
2022-02-27 13:04:26 +0000

    .

--- src/components/DisplayMaps.js
+++ src/components/DisplayMaps.js
@@ -14,6 +14,84 @@ export default function DisplayMaps() {
 
   // useEffect(() => {}, []);
 
+  const addMarker = (lat, long, alt = 0, map, mui, rotateDegree = 0) => {
+    // var LocationOfMarker = { lat: lat, lng: long, alt: alt };
+
+    //console.log(lat, long, alt);
+
+    // var minDist = 1000,
+    //   markerDist,
+    //   objects = map.getObjects(),
+    //   len = map.getObjects().length,
+    //   i;
+
+    // for (i = 0; i < len; i += 1) {
+    //   markerDist = objects[i].getGeometry().distance({ lat: lat, lng: long });
+    //   if (markerDist < minDist) {
+    //     minDist = markerDist;
+    //     console.log(objects[i], " objects[i]");
+    //     map.removeObject(objects[i]);
+    //   }
+    // }
+
+    // var icon = new H.map.Icon("../assets/plane/plane.png");
+
+    var domIconElement = document.createElement("div"),
+      interval = 0;
+
+    domIconElement.innerHTML =
+      '<img src="../assets/plane/plane.png" width="40px" />';
+    var point = new H.map.DomMarker(
+      { lat: lat, lng: long },
+      {
+        icon: new H.map.DomIcon(domIconElement, {
+          onAttach: function (clonedElement, domIcon, domMarker) {
+            var clonedContent = clonedElement.getElementsByTagName("img")[0];
+            // console.log(clonedElement);
+
+            clonedContent.style.transform = "rotate(" + rotateDegree + "deg)";
+
+            // set interval to rotate icon's content by 45 degrees every second.
+            //   interval = setInterval(function () {
+            //     clonedContent.style.transform =
+            //       "rotate(" + (counter += 45) + "deg)";
+            //   }, 5000);
+          },
+          onDetach: function (clonedElement, domIcon, domMarker) {
+            // stop the rotation if dom icon is not in map's viewport
+            clearInterval(interval);
+          },
+        }),
+      }
+    );
+    // point.setPosition({ lat: number, lng: number });
+
+    //map.removeObject(point);
+    var marker = map.addObject(point);
+
+    // var marker = new H.map.Marker(LocationOfMarker, {
+    //   icon: icon,
+    // });
+
+    map.addObject(marker);
+    marker.addEventListener(
+      "tap",
+      function (evt) {
+        var bubble = new H.ui.InfoBubble(
+          { lat: lat, lng: long },
+          {
+            content:
+              "<div>Flight Info</div>" +
+              "<div>from : <br /> to : <br />Capacity: 55,097</div>",
+          }
+        );
+
+        mui.addBubble(bubble);
+      },
+      false
+    );
+  };
+
   const loadPlanes = useCallback(
     async (hMap, myui) => {
       const response = await PlaneService.getPlanes();
@@ -98,84 +176,6 @@ export default function DisplayMaps() {
     setMap(hMap);
     console.log(map);
   }, [H.Map, H.service.Platform, H.ui.UI, loadPlanes, map, ui]);
-
-  const addMarker = (lat, long, alt = 0, map, mui, rotateDegree = 0) => {
-    // var LocationOfMarker = { lat: lat, lng: long, alt: alt };
-
-    //console.log(lat, long, alt);
-
-    // var minDist = 1000,
-    //   markerDist,
-    //   objects = map.getObjects(),
-    //   len = map.getObjects().length,
-    //   i;
-
-    // for (i = 0; i < len; i += 1) {
-    //   markerDist = objects[i].getGeometry().distance({ lat: lat, lng: long });
-    //   if (markerDist < minDist) {
-    //     minDist = markerDist;
-    //     console.log(objects[i], " objects[i]");
-    //     map.removeObject(objects[i]);
-    //   }
-    // }
-
-    // var icon = new H.map.Icon("../assets/plane/plane.png");
-
-    var domIconElement = document.createElement("div"),
-      interval = 0;
-
-    domIconElement.innerHTML =
-      '<img src="../assets/plane/plane.png" width="40px" />';
-    var point = new H.map.DomMarker(
-      { lat: lat, lng: long },
-      {
-        icon: new H.map.DomIcon(domIconElement, {
-          onAttach: function (clonedElement, domIcon, domMarker) {
-            var clonedContent = clonedElement.getElementsByTagName("img")[0];
-            // console.log(clonedElement);
-
-            clonedContent.style.transform = "rotate(" + rotateDegree + "deg)";
-
-            // set interval to rotate icon's content by 45 degrees every second.
-            //   interval = setInterval(function () {
-            //     clonedContent.style.transform =
-            //       "rotate(" + (counter += 45) + "deg)";
-            //   }, 5000);
-          },
-          onDetach: function (clonedElement, domIcon, domMarker) {
-            // stop the rotation if dom icon is not in map's viewport
-            clearInterval(interval);
-          },
-        }),
-      }
-    );
-    // point.setPosition({ lat: number, lng: number });
-
-    //map.removeObject(point);
-    var marker = map.addObject(point);
-
-    // var marker = new H.map.Marker(LocationOfMarker, {
-    //   icon: icon,
-    // });
-
-    map.addObject(marker);
-    marker.addEventListener(
-      "tap",
-      function (evt) {
-        var bubble = new H.ui.InfoBubble(
-          { lat: lat, lng: long },
-          {
-            content:
-              "<div>Flight Info</div>" +
-              "<div>from : <br /> to : <br />Capacity: 55,097</div>",
-          }
-        );
-
-        mui.addBubble(bubble);
-      },
-      false
-    );
-  };
 
   // const addMarkerToGroup = (group, coordinate, html) => {
   //   var marker = new H.map.Marker(coordinate);
